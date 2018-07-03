d04813b4f6fd0afbc2c2f8f57b4c9f9375657ede
mpmunasinghe <mpmunasinghe@gmail.com>
2018-07-03 11:25:08 +0530

    Adding java sample

---
+++ actions/nodejs/newly.js
@@ -0,0 +1,19 @@
+/**
+  *
+  * main() will be invoked when you Run This Action.
+  *
+  * @param Cloud Functions actions accept a single parameter,
+  *        which must be a JSON object.
+  *
+  * In this case, the params variable will look like:
+  *     { "name": "xxxx" }
+  *
+  * @return which must be a JSON object.
+  *         It will be the output of this action.
+  *
+  */
+function main(params) {
+    return { gitUrl : "https://github.com/" + params.gitUrl + "/" + params.manifestPath, manifestPath : "." };
+}
+
+exports.main = main;
